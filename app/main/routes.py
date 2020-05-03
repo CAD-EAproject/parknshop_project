6105c33430c0f7db1d07f874c1d81638bfc7e27f
ChanWaiLok01 <60951513+ChanWaiLok01@users.noreply.github.com>
2020-05-03 21:56:56 +0800

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -77,20 +77,101 @@ def index():
                            newproducts=newproducts.items, promotions=promotions, features=features,
                            pdbrands=pdbrands.items, page=page, productform=productform, reviews=reviews)
 
+@bp.route('/edit_banner/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_banner(id):
+    bannerid = Banner.query.get_or_404(id)
+    form = BannerForm(request.form)
+    if form.validate_on_submit():
+        bannerid.banner = form.banner.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        form.banner.data = bannerid.banner
+    return render_template('edit_banner.html', title=_('Edit Categories'),
+                           form=form, bannerid=bannerid)
 
-@bp.route('/user/<username>')
+@bp.route('/delete_banner/<int:id>')
+@login_required
+def delete_banner(id):
+    bannerid = Banner.query.get_or_404(id)
+    db.session.delete(bannerid)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@bp.route('/edit_promotion/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_promotion(id):
+    prmotionid = Promotion.query.get_or_404(id)
+    form = PromotionForm(request.form)
+    if form.validate_on_submit():
+        prmotionid.name = form.name.data
+        prmotionid.url = form.url.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        form.name.data = prmotionid.name
+        form.url.data = prmotionid.url
+    return render_template('edit_promotion.html', title=_('Edit Categories'),
+                           form=form, prmotionid=prmotionid)
+
+@bp.route('/delete_promotion/<int:id>')
+@login_required
+def delete_promotion(id):
+    promotionid = Promotion.query.get_or_404(id)
+    promotionid.products = []
+    db.session.commit()
+    db.session.delete(promotionid)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@bp.route('/show_promotion/<int:id>')
+def show_promotion(id):
+    showpromotion = Promotion.query.get_or_404(id)
+    breadcrumb = Promotion.query.filter_by(id=showpromotion.id)
+    categoriess = Categories.query.order_by(Categories.categories)
+    subcats = SubCategories.query.order_by(SubCategories.subcategories)
+    page = request.args.get('page', 1, type=int)
+    promotionitem = showpromotion.product.paginate(
+        page, current_app.config['PRODUCT_PER_PAGE'], False)
+    next_url = url_for('main.show_promotion', id=showpromotion.id, page=promotionitem.next_num) \
+        if promotionitem.has_next else None
+    prev_url = url_for('main.show_promotion', id=showpromotion.id, page=promotionitem.prev_num) \
+        if promotionitem.has_prev else None
+    return render_template("show_promotion.html", title=_('Promotion'), promotionitem=promotionitem.items,
+                           breadcrumb=breadcrumb, categoriess=categoriess, subcats=subcats,
+                           page=page, next_url=next_url, prev_url=prev_url)
+
+@bp.route('/user/<username>', methods=['GET', 'POST'])
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+    postform = PostForm()
+    if postform.validate_on_submit():
+        post = Post(body=postform.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash(_('Your post is now live!'))
+        return redirect(url_for('main.user', username=current_user.username))
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+                           next_url=next_url, prev_url=prev_url, postform=postform)
+
+@bp.route('/delete_post/<int:id>')
+@login_required
+def delete_post(id):
+    postid = Post.query.get_or_404(id)
+    db.session.delete(postid)
+    db.session.commit()
+    return redirect(url_for('main.user', username=current_user.username))
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -140,3 +221,29 @@ def unfollow(username):
     db.session.commit()
     flash(_('You are not following %(username)s.', username=username))
     return redirect(url_for('main.user', username=username))
+
+@bp.route('/edit_feature/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_feature(id):
+    featureid = Feature.query.get_or_404(id)
+    form = FeatureForm(request.form)
+    if form.validate_on_submit():
+        featureid.title = form.title.data
+        featureid.description = form.description.data
+        featureid.url = form.url.data
+        db.session.commit()
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        form.title.data = featureid.title
+        form.description.data = featureid.description
+        form.url.data = featureid.url
+    return render_template('edit_feature.html', title=_('Edit Feature'), form=form)
+
+@bp.route('/delete_feature/<int:id>')
+@login_required
+def delete_feature(id):
+    featureid = Feature.query.get_or_404(id)
+    db.session.delete(featureid)
+    db.session.commit()
+    return redirect(url_for('main.index'))
